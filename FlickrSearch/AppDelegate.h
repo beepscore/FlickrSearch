bef442e01693c6bcac21fc3f4aed177c5220c3f3
beepscore <support@beepscore.com>
2014-04-17 19:27:37 -0700

    Initial Commit

---
+++ FlickrSearch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FlickrSearch
+//
+//  Created by Steve Baker on 4/17/14.
+//  Copyright (c) 2014 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FlickrSearch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FlickrSearch
+//
+//  Created by Steve Baker on 4/17/14.
+//  Copyright (c) 2014 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
