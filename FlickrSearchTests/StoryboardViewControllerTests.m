23555704a3d5b76e56eb515792a0abeac31396f7
beepscore <support@beepscore.com>
2014-04-18 11:35:07 -0700

    Add StoryboardViewControllerTests. In storyboards ViewController add identifier.

---
+++ FlickrSearchTests/StoryboardViewControllerTests.m
@@ -0,0 +1,43 @@
+//
+//  StoryboardViewControllerTests.m
+//  FlickrSearch
+//
+//  Created by Steve Baker on 4/18/14.
+//  Copyright (c) 2014 Beepscore LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "ViewController.h"
+
+@interface StoryboardViewControllerTests : XCTestCase
+@property ViewController *vc;
+@end
+
+@implementation StoryboardViewControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+
+    // http://iosunittesting.com/using-storyboards/
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main_iPhone"
+                                                         bundle:[NSBundle mainBundle]];
+
+    self.vc = [storyboard instantiateViewControllerWithIdentifier:@"ViewController"];
+    [self.vc performSelectorOnMainThread:@selector(loadView)
+                              withObject:nil
+                           waitUntilDone:YES];
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    //XCTAssertNotNil(self.vc.textField.delegate);
+}
+
+@end
