8685594c9a6bf0da5311b07fa17ac9099ab7477a
beepscore <support@beepscore.com>
2014-04-18 11:41:52 -0700

    Add BSStoryboardViewControllerLoader

---
+++ FlickrSearchTests/BSStoryboardViewControllerLoader.h
@@ -0,0 +1,13 @@
+//
+//  BSStoryboardViewControllerLoader.h
+//  FlickrSearch
+//
+//  Created by Steve Baker on 4/18/14.
+//  Copyright (c) 2014 Beepscore LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BSStoryboardViewControllerLoader : NSObject
+
+@end
---
+++ FlickrSearchTests/BSStoryboardViewControllerLoader.m
@@ -0,0 +1,13 @@
+//
+//  BSStoryboardViewControllerLoader.m
+//  FlickrSearch
+//
+//  Created by Steve Baker on 4/18/14.
+//  Copyright (c) 2014 Beepscore LLC. All rights reserved.
+//
+
+#import "BSStoryboardViewControllerLoader.h"
+
+@implementation BSStoryboardViewControllerLoader
+
+@end
