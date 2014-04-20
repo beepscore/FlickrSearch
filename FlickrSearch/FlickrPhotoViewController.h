8af4a3db0f5d4e97e84f3dcfe6e56eb3f0d44f30
beepscore <support@beepscore.com>
2014-04-19 20:53:31 -0700

    Add FlickrPhotoViewController

---
+++ FlickrSearch/FlickrPhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  FlickrPhotoViewController.h
+//  FlickrSearch
+//
+//  Created by Steve Baker on 4/19/14.
+//  Copyright (c) 2014 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlickrPhotoViewController : UIViewController
+
+@end
