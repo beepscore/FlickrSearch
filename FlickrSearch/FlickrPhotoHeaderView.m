3bc2cb8501d9088b92b8989eda581894193b814f
beepscore <support@beepscore.com>
2014-04-19 18:21:08 -0700

    Add FlickrPhotoHeaderViewPrivate.h

--- FlickrSearch/FlickrPhotoHeaderView.m
+++ FlickrSearch/FlickrPhotoHeaderView.m
@@ -7,6 +7,7 @@
 //
 
 #import "FlickrPhotoHeaderView.h"
+#import "FlickrPhotoHeaderViewPrivate.h"
 
 @implementation FlickrPhotoHeaderView
 
---
+++ FlickrSearch/FlickrPhotoHeaderViewPrivate.h
@@ -0,0 +1,12 @@
+//
+//  FlickrPhotoHeaderViewPrivate.h
+//  FlickrSearch
+//
+//  Created by Steve Baker on 4/19/14.
+//  Copyright (c) 2014 Beepscore LLC. All rights reserved.
+//
+
+@interface FlickrPhotoHeaderView ()
+@property(weak) IBOutlet UIImageView *backgroundImageView;
+@property(weak) IBOutlet UILabel *searchLabel;
+@end
