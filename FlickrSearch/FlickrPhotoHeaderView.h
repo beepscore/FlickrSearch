4b7693c1506131160b8e3249cf647ba1f155aafe
beepscore <support@beepscore.com>
2014-04-19 18:12:39 -0700

    Add FlickrPhotoHeaderView.

---
+++ FlickrSearch/FlickrPhotoHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  FlickrPhotoHeaderView.h
+//  FlickrSearch
+//
+//  Created by Steve Baker on 4/19/14.
+//  Copyright (c) 2014 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlickrPhotoHeaderView : UICollectionReusableView
+
+@end
